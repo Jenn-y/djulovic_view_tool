00a5ba2bfce686ca395f22ed1bffa5821aaef2fc
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2020-07-19 13:39:58 +0200

    initial commit

---
+++ lib/djulovic_view_tool.rb
@@ -0,0 +1,7 @@
+require "djulovic_view_tool/version"
+require "djulovic_view_tool/renderer"
+
+module DjulovicViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/djulovic_view_tool/version.rb
@@ -0,0 +1,3 @@
+module DjulovicViewTool
+  VERSION = "0.1.0"
+end
